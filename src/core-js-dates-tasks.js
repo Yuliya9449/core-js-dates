f192fb1ca429f1333e9e2c562145cbd994e8c443
Yuliya Kotova <yulya9449@yandex.ru>
2024-03-06 03:31:43 +0300

    feat: add solution getCountDaysOnPeriod task

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -113,8 +113,12 @@ function getCountDaysInMonth(month, year) {
  * '2024-02-01T00:00:00.000Z', '2024-02-12T00:00:00.000Z'  => 12
  */
 //! 6
-function getCountDaysOnPeriod(/* dateStart, dateEnd */) {
-  throw new Error('Not implemented');
+function getCountDaysOnPeriod(dateStart, dateEnd) {
+  const millisecondsPerDay = 24 * 60 * 60 * 1000;
+
+  return Math.floor(
+    (Date.parse(dateEnd) - Date.parse(dateStart)) / millisecondsPerDay + 1
+  );
 }
 
 /**
